2e173808be30431ac21fb55e0f74678cd14ee6ee
Daniel <danielepv8@gmail.com>
2026-01-26 00:55:05 -0600

    Update CORS configuration to specify allowed origins for frontend access

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        env('FRONTEND_URL', '*'),
+        'https://tu-proyecto.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
